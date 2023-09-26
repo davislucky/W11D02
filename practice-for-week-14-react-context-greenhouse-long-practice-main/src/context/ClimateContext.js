da219117283d589adf97f17e4250193c2bbc02e0
davislucky <58579323+davislucky@users.noreply.github.com>
2023-09-26 13:51:23 -0700

    Add Climate Context

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -1,2 +1,22 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { useContext, createContext, useState } from "react";
+
+
+const ClimateContext = createContext();
+
+export default function ClimateProvider (props) {
+    const [temp, setTemp] = useState(50);
+    // const [humidity, setHumidity] = useState(.4)
+
+    return (
+        <ClimateContext.Provider value={{temp, setTemp}}>
+            {props.children}
+        </ClimateContext.Provider>
+    );
+};
+
+export const useClimateProvider = () => {
+    return useContext(ClimateContext);
+}
